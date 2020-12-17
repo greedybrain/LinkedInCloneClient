0a4c756a8d620a53e0373fef0116a89469cf7b92
greedybrain <greedybrain18@gmail.com>
2020-12-17 08:23:11 -0500

    ...

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
         render() {
                 return (
                         <header className="main_header">
-                                
+                                Hello
                         </header>
                 )
         }
