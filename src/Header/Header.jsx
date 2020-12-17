abbabdb27f3fd51009e42d32e1bc4d67319c635c
greedybrain <greedybrain18@gmail.com>
2020-12-17 14:53:13 -0500

    added Logo, SearchBar, Navbar comp to Header comp

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import SearchBar from './SearchBar'
 
 export default class Header extends Component {
         render() {
                 return (
                         <header className="main_header">
-                                Hello
+                                <Logo />
+                                <SearchBar />
+                                <Navbar />
                         </header>
                 )
         }
