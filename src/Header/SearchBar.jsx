5a2fed69ef7237fd28acdff9dd64c14f024f5bd6
greedybrain <greedybrain18@gmail.com>
2020-12-17 14:55:06 -0500

    added form to SearchBar comp

--- src/Header/SearchBar.jsx
+++ src/Header/SearchBar.jsx
@@ -1,9 +1,28 @@
+//! Core Modules
 import React from 'react'
+
+//! Custom Modules / Components
+import DivWithInput from '../Common/DivWithInput'
+import Icon from '../Common/Icon'
+import '../Styles/SearchBar.css'
 
 const SearchBar = () => {
         return (
-                <div>
-                        
+                <div className="search_bar_wrapper">
+                        <form>
+                                <div className="search_icon_and_input">
+                                        <Icon name="fas fa-search" />
+                                        <DivWithInput 
+                                                specialClass="search" 
+                                                inputType="search" 
+                                                inputName="search" 
+                                                inputPlaceholder="Search" 
+                                                inputValue={null} 
+                                                inputeRequired={true} 
+                                                focus={true} 
+                                        />
+                                </div>
+                        </form>
                 </div>
         )
 }
