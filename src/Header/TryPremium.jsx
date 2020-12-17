82178839dc3babe8e7d8748e72e263d203f19d33
greedybrain <greedybrain18@gmail.com>
2020-12-17 07:11:25 -0500

    created TryPremium comp

---
+++ src/Header/TryPremium.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TryPremium = () => {
+        return (
+                <div>
+                        
+                </div>
+        )
+}
+
+export default TryPremium
