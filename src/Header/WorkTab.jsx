37302ef33f8808708cd80e9936e18107733ea7b2
greedybrain <greedybrain18@gmail.com>
2020-12-17 07:11:40 -0500

    created WorkTab comp

---
+++ src/Header/WorkTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkTab = () => {
+        return (
+                <div>
+                        
+                </div>
+        )
+}
+
+export default WorkTab
