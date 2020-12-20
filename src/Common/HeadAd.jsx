7947b3f421e9478fffdeb8d6749380fd8d15286c
greedybrain <greedybrain18@gmail.com>
2020-12-20 08:53:37 -0500

    added HeadAd comp logic

---
+++ src/Common/HeadAd.jsx
@@ -0,0 +1,27 @@
+//! Core Modules
+import React from 'react'
+
+//! Custom Modules / Components
+//
+
+const HeadAd = () => {
+        return (
+                <div className="head_ad_wrapper" style={{textAlign: 'center', padding: '5px 0', cursor: 'pointer'}}>
+                        <div className="head_ad">
+                                <p style={{textDecoration: 'underline', display: 'inline-block', fontSize: '.85rem'}}>
+                                        <strong>
+                                                <span style={{color: '#0a66c2', textDecoration: 'underline'}}>WE'RE SO DISAPPOINTED</span>
+                                        </strong>
+                                        <strong> 
+                                                - We really want to show you why so many healthcare professionals prefer BHG!
+                                        </strong>
+                                </p>
+                                <span style={{color: '#00000099', marginLeft: '10px', fontSize: '.8rem'}}>
+                                        Ad
+                                </span>
+                        </div>
+                </div>
+        )
+}
+
+export default HeadAd
