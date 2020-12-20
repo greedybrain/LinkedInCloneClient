2736a4d0ab0d230af2e8b7a1504209cf3b81008e
greedybrain <greedybrain18@gmail.com>
2020-12-20 08:51:12 -0500

    added Routes comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+//! Core modules
 import React, { Component } from 'react';
+
+//! Custom Modules/ Components
 import Header from './Header/Header';
+import Routes from './Routes';
 
 class App extends Component {
         render() {
                 return (
                         <div className="App">
                                 <Header />
+                                <Routes />
                         </div>
                 );
         }
