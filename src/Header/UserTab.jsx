1335043897b9e10b5ae9e85f3041b43f5d60ee86
greedybrain <greedybrain18@gmail.com>
2020-12-20 08:55:21 -0500

    added UserTab logic

--- src/Header/UserTab.jsx
+++ src/Header/UserTab.jsx
@@ -1,9 +1,31 @@
+//! Core Modules
 import React from 'react'
+
+//! Cutome Modules/ Components
+import UserImage from '../Common/UserImage'
+import '../Styles/UserTab.css'
+
+const userImage = {
+        imageWrapper: {
+                width: "20px",
+                image: {
+                        borderRadius: "50%"
+                }
+        }
+}
 
 const UserTab = () => {
         return (
-                <div>
-                        
+                <div className="user_tab_wrapper" style={null}>
+                        <UserImage styles={userImage} />
+                        <div className="user_tab_lower">
+                                <div className="me">
+                                        Me
+                                </div>
+                                <div className="drop_down_arrow">
+                                        &#9662;
+                                </div>
+                        </div>
                 </div>
         )
 }
