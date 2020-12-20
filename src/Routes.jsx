2affa52d67fcf9866ae8095f887ded22d4e5c562
greedybrain <greedybrain18@gmail.com>
2020-12-20 08:53:51 -0500

    created first route which is the home route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home/Home'
 
 const Routes = () => {
         return (
-                <div>
-                        
-                </div>
+                <Switch>
+                        <Route
+                                exact
+                                path='/'
+                                render={routerProps => <Home {...routerProps} />}
+                        />
+                </Switch>
         )
 }
 
