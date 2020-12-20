4aa5b235eed327201bee20a399b10c4cabdf6ecb
greedybrain <greedybrain18@gmail.com>
2020-12-20 08:55:31 -0500

    added WorkTab logic

--- src/Header/WorkTab.jsx
+++ src/Header/WorkTab.jsx
@@ -1,9 +1,22 @@
+//! Core modules
 import React from 'react'
+
+//! Custom Modules/ Components
+import Icon from '../Common/Icon'
+import '../Styles/WorkTab.css'
 
 const WorkTab = () => {
         return (
-                <div>
-                        
+                <div className="work_tab_wrapper">
+                        <Icon name="fas fa-th" />
+                        <div className="work_tab_lower">
+                                <div className="work">
+                                        Work
+                                </div>
+                                <div className="drop_down_arrow">
+                                        &#9662;
+                                </div>
+                        </div>
                 </div>
         )
 }
