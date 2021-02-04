3960d121fffc7351caea55003e2c28d926439c25
greedybrain <greedybrain18@gmail.com>
2021-02-04 00:41:19 -0500

    ...

--- src/Common/DivWithInput.jsx
+++ src/Common/DivWithInput.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
+import React from "react";
 
+const DivWithInput = ({
+	specialClass,
+	inputType,
+	inputName,
+	inputPlaceholder,
+	inputValue,
+	inputRequired,
+	handleChange,
+	handleFocus,
+	handleBlur,
+	focus,
+}) => {
+	return (
+		<div className={specialClass + " important_input_styles"}>
+			<input
+				type={inputType}
+				name={inputName}
+				placeholder={inputPlaceholder}
+				value={inputValue}
+				required={inputRequired}
+				onChange={handleChange}
+				autoFocus={focus}
+				onFocus={handleFocus}
+				onBlur={handleBlur}
+			/>
+		</div>
+	);
+};
 
-const DivWithInput = ({ specialClass, inputType, inputName, inputPlaceholder, inputValue, inputRequired, handleChange, handleFocus, handleBlur, focus }) => {
-        return (
-                <div 
-                        className={specialClass + ' important_input_styles'}
-                >
-                        <input 
-                                type={inputType} 
-                                name={inputName} 
-                                placeholder={inputPlaceholder} 
-                                value={inputValue} 
-                                required={inputRequired} 
-                                onChange={handleChange}
-                                autoFocus={focus}
-                                onFocus={handleFocus}
-                                onBlur={handleBlur}
-                        />
-                </div>
-        )
-}
-
-export default DivWithInput
+export default DivWithInput;
